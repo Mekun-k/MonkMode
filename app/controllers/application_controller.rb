d8915ff601cf5d5d2ed481203fda804d5a1757a5
Mekun-k <akaidonnbei@gmail.com>
2023-12-26 07:24:41 +0900

    Add: ページ関連を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  #ログイン後の遷移先
+  def after_sign_in_path_for(resource_or_scope)
+    root_path
+  end
+
+  #ログアウト後の遷移先
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
