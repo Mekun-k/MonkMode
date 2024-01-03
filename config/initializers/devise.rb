84cae4fc9feb8387b2a9d87600282600e77610e8
Mekun-k <akaidonnbei@gmail.com>
2024-01-03 16:20:19 +0900

    Destory: 記述内容に誤りがあったので修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -188,7 +188,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = 3.minutes
+  # config.timeout_in = 3.minutes
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
@@ -204,8 +204,8 @@ Devise.setup do |config|
   # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
   # :both  = Enables both strategies
   # :none  = No unlock strategy. You should handle unlocking by yourself.
-  config.unlock_strategy = :email
-  config.maximum_attempts = 20
+  # config.unlock_strategy = :email
+  # config.maximum_attempts = 20
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   # config.unlock_in = 1.hour
