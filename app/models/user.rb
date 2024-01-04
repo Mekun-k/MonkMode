360fcf1307f797e48991c8370fe0349a586c4f95
Mekun-k <akaidonnbei@gmail.com>
2024-01-04 17:15:57 +0900

    Fix: ソーシャルログイン時、メール認証を発動させないよう修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,27 +8,32 @@ class User < ApplicationRecord
 
   has_many :sns_credential, dependent: :destroy
 
+  def skip_confirmation!
+    self.confirmed_at = Time.now
+  end
+
   private
 
-  #パスワードなしでユーザー編集するためのメソッド
-  # def update_without_current_password(params, *options)
-  #   params.delete(:current_password)
+  # パスワードなしでユーザー編集するためのメソッド
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
 
-  #   if params[:password].blank? && params[:password_confirmation].blank?
-  #     params.delete(:password)
-  #     params.delete(:password_confirmation)
-  #   end
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
 
-  #   result = update_attributes(params, *options)
-  #   clean_up_passwords
-  #   result
-  # end
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token(10)
+      user.skip_confirmation!
     end
   end
 end
