dbff4aada3b1878424bdc6f8be49f94080f7e934
Mekun-k <akaidonnbei@gmail.com>
2024-01-13 16:20:28 +0900

    Fix: 1を定数で定義

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,25 +2,25 @@ class Answer < ApplicationRecord
   belongs_to :user
   has_many :child_answers, dependent: :destroy
 
-  def self.score_create(answer)
+  NUMBERSET = 1
 
+  def self.score_create(answer)
     child_answers = answer.child_answers
     content = child_answers.pluck(:content)
 
     if content.count == 15
-      success_result = content.count(true) * 1
-      failure_result = content.count(false) * -1
+      success_result = content.count(true) * NUMBERSET
+      failure_result = content.count(false) * -NUMBERSET
       answer.score = success_result + failure_result
       answer.save
     else
       is_error = true
     end
-
   end
 
   def self.success_result(answer)
     child_answers = answer.child_answers
     content = child_answers.pluck(:content)
-    result = content.count(true) * 1
+    result = content.count(true) * NUMBERSET
   end
 end
