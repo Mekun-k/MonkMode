330a59c37c48c5bb6073ac923a27819e4fbd93ed
Mekun-k <akaidonnbei@gmail.com>
2024-01-15 07:28:40 +0900

    Add: プロフィール画像を登録できるように実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   #ユーザー登録時、任意のカラムを登録できる設定
   def configure_permitted_parameters
-    added_attrs = [ :email, :password, :password_confirmation, :name ]
+    added_attrs = [ :email, :password, :password_confirmation, :name, :avatar ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
+  has_one_attached :avatar
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
