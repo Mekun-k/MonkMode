47c365db34d672214fd5df9eef7f4e461fd3f90e
Mekun-k <akaidonnbei@gmail.com>
2024-01-14 20:50:54 +0900

    Add: プロフィール、フォローフォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,12 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
